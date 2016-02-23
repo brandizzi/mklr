93afa3a3eced2afe42c9241288a0b47947e8a6f7
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-02-23 09:08:21 -0300

    #1 Documentation for Repository.
    Doctests for the constructor and checkout(). We caught a bug,
    actually, so the change in checkout() code is a fix.

--- mklr/repository.py
+++ mklr/repository.py
@@ -27,11 +27,43 @@ class Repository(object):
     """
 
     def __init__(self, location):
+        """
+        A ``Repository`` needs a location, which can be a path to a directory
+        containing the repository.
+
+        >>> from mklr.tests.util import repo1_dir
+        >>> r = Repository(repo1_dir())
+        """
         self.location = location
         self.repo = git.Repo(location)
 
     def checkout(self, branch):
+        """
+        ``Repository.checkout()`` check outs the given branch. Given, for
+        example, the repository below::
+
+        >>> from mklr.tests.util import repo1_dir
+        >>> repo_path = repo1_dir()
+        >>> r = Repository(repo_path)
+
+        ...then we can check out different branches::
+
+        >>> r.checkout('master')
+        >>> import os.path
+        >>> r.get_head().message
+        u'Fifth master commit.\\n'
+        >>> with file(os.path.join(repo_path, 'file')) as f:
+        ...     f.read()
+        'master 5\\n'
+        >>> r.checkout('branch1')
+        >>> r.get_head().message
+        u'Second branch1 commit.\\n'
+        >>> with file(os.path.join(repo_path, 'file')) as f:
+        ...     f.read()
+        'branch1 2\\n'
+        """
         self.repo.head.reference = branch
+        self.repo.head.reset(index=True, working_tree=True)
 
     def get_head(self):
         commit = self.repo.head.commit
